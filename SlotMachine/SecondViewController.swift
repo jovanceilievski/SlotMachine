feadfffa5729adfeafc004f9757402f2e7aeb373
CodeWell <tanjap@hotmail.com>
2018-10-15 20:24:38 +0200

    function-deleted

--- SlotMachine/SecondViewController.swift
+++ SlotMachine/SecondViewController.swift
@@ -15,7 +15,7 @@ class SecondViewController: UIViewController {
     @IBOutlet var image3 : UIImageView!
     @IBOutlet var image4 : UIImageView!
     @IBOutlet var image5 : UIImageView!
-    var imageArray : [UIImage] = [#imageLiteral(resourceName: "Orange-512"), #imageLiteral(resourceName: "51dMi2iYAVL"),#imageLiteral(resourceName: "Slot-Machine-Lemon-Stitched-5_5-Inch"), #imageLiteral(resourceName: "Grapes-512"), #imageLiteral(resourceName: "Bell-512"), #imageLiteral(resourceName: "power-stars-f1"), #imageLiteral(resourceName: "Bananas-512"), #imageLiteral(resourceName: "Strawberry-512"), #imageLiteral(resourceName: "pear09062012"), #imageLiteral(resourceName: "Cherry-512")]
+    var imageArray : [UIImage] = [#imageLiteral(resourceName: "Orange-512"), #imageLiteral(resourceName: "Grapes-512"), #imageLiteral(resourceName: "power-stars-f1"), #imageLiteral(resourceName: "Bananas-512"), #imageLiteral(resourceName: "Strawberry-512"), #imageLiteral(resourceName: "Cherry-512")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,31 +26,6 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func getFirstRandomImage(){
-        let randomIndex = Int (arc4random_uniform(UInt32(imageArray.count)))
-        image1.image = imageArray[randomIndex]
-    }
-    
-    func getSecondRandomImage(){
-        let randomIndex = Int (arc4random_uniform(UInt32(imageArray.count)))
-        image2.image = imageArray[randomIndex]
-    }
-    
-    func getThirdRandomImage(){
-        let randomIndex = Int (arc4random_uniform(UInt32(imageArray.count)))
-        image3.image = imageArray[randomIndex]
-    }
-    
-    func getFourthRandomImage(){
-        let randomIndex = Int (arc4random_uniform(UInt32(imageArray.count)))
-        image4.image = imageArray[randomIndex]
-    }
-    
-    func getFifthRandomImage(){
-        let randomIndex = Int (arc4random_uniform(UInt32(imageArray.count)))
-        image5.image = imageArray[randomIndex]
     }
     
     func createAnimationImage (imageView: UIImageView)  {
@@ -65,8 +40,9 @@ class SecondViewController: UIViewController {
         imageView.animationDuration = 2
         imageView.animationRepeatCount = 2
         imageView.startAnimating()
-     
-    }
+        
+        let randomIndex = Int (arc4random_uniform(UInt32(imageArray.count)))
+        imageView.image = imageArray[randomIndex]    }
     
     @IBAction func spin () {
         createAnimationImage(imageView: image1)
@@ -74,11 +50,6 @@ class SecondViewController: UIViewController {
         createAnimationImage(imageView: image3)
         createAnimationImage(imageView: image4)
         createAnimationImage(imageView: image5)
-        getFirstRandomImage()
-        getSecondRandomImage()
-        getThirdRandomImage()
-        getFourthRandomImage()
-        getFifthRandomImage()
         
     }
 }
