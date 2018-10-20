10a2e34fa5239335ce0f9631ea7342bd25c48006
Jovance Ilievski <>
2018-10-20 13:36:23 +0200

    added pictures

--- SlotMachine/SecondViewController.swift
+++ SlotMachine/SecondViewController.swift
@@ -15,6 +15,16 @@ class SecondViewController: UIViewController {
     @IBOutlet var image3 : UIImageView!
     @IBOutlet var image4 : UIImageView!
     @IBOutlet var image5 : UIImageView!
+    @IBOutlet var image6 : UIImageView!
+    @IBOutlet var image7 : UIImageView!
+    @IBOutlet var image8 : UIImageView!
+    @IBOutlet var image9 : UIImageView!
+    @IBOutlet var image10 : UIImageView!
+    @IBOutlet var image11 : UIImageView!
+    @IBOutlet var image12 : UIImageView!
+    @IBOutlet var image13 : UIImageView!
+    @IBOutlet var image14 : UIImageView!
+    @IBOutlet var image15 : UIImageView!
     var imageArray : [UIImage] = [#imageLiteral(resourceName: "Orange-512"), #imageLiteral(resourceName: "Grapes-512"), #imageLiteral(resourceName: "power-stars-f1"), #imageLiteral(resourceName: "Bananas-512"), #imageLiteral(resourceName: "Strawberry-512"), #imageLiteral(resourceName: "Cherry-512")]
     
     override func viewDidLoad() {
@@ -22,6 +32,7 @@ class SecondViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -50,6 +61,17 @@ class SecondViewController: UIViewController {
         createAnimationImage(imageView: image3)
         createAnimationImage(imageView: image4)
         createAnimationImage(imageView: image5)
+        createAnimationImage(imageView: image6)
+        createAnimationImage(imageView: image7)
+        createAnimationImage(imageView: image8)
+        createAnimationImage(imageView: image9)
+        createAnimationImage(imageView: image10)
+        createAnimationImage(imageView: image11)
+        createAnimationImage(imageView: image12)
+        createAnimationImage(imageView: image13)
+        createAnimationImage(imageView: image14)
+        createAnimationImage(imageView: image15)
         
     }
 }
+
